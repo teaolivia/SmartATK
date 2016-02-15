160b8da942744046def50e7556c3ced3c8a0125b
Calvin Aditya Jonathan <deus.caj@gmail.com>
2016-02-15 23:19:36 +0700

    Pushed newest sql dump

--- SQL/smartatk.sql
+++ SQL/smartatk.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 06, 2016 at 01:53 PM
+-- Generation Time: Feb 15, 2016 at 05:18 PM
 -- Server version: 10.1.8-MariaDB-log
 -- PHP Version: 5.6.14
 
@@ -37,7 +37,8 @@ CREATE TABLE `t_atk` (
 --
 
 INSERT INTO `t_atk` (`kode_barang`, `nama_barang`, `stok`) VALUES
-('A01', 'Snowman Permanent Marker', 300);
+('A01', 'Spidol Merah', 60),
+('B02', 'A4', 5);
 
 -- --------------------------------------------------------
 
@@ -46,14 +47,13 @@ INSERT INTO `t_atk` (`kode_barang`, `nama_barang`, `stok`) VALUES
 --
 
 CREATE TABLE `t_pemakaian` (
-  `id_pemakaian` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   `nama_user` varchar(50) NOT NULL,
   `kategori_user` varchar(16) NOT NULL,
   `n_pakai` int(11) NOT NULL,
   `kode_barang` varchar(8) NOT NULL,
   `nama_barang` varchar(32) NOT NULL,
-  `deskripsi` varchar(50) DEFAULT NULL,
+  `deskripsi` varchar(50) NOT NULL,
   `tanggal` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -61,9 +61,12 @@ CREATE TABLE `t_pemakaian` (
 -- Dumping data for table `t_pemakaian`
 --
 
-INSERT INTO `t_pemakaian` (`id_pemakaian`, `id_user`, `nama_user`, `kategori_user`, `n_pakai`, `kode_barang`, `nama_barang`, `deskripsi`, `tanggal`) VALUES
-(1, 1, 'Calvin', 'Mahasiswa', 100, 'A01', 'Snowman Permanent Marker', 'asdasd', '2016-01-27'),
-(2, 1, 'Calvin', 'Mahasiswa', 100, 'A01', 'Snowman Permanent Marker', 'asdasd', '2016-01-27');
+INSERT INTO `t_pemakaian` (`id_user`, `nama_user`, `kategori_user`, `n_pakai`, `kode_barang`, `nama_barang`, `deskripsi`, `tanggal`) VALUES
+(1, 'Calvin', 'Mahasiswa', 25, 'A01', 'Spidol Merah', 'Kelas', '2016-02-15'),
+(1, 'Calvin', 'Mahasiswa', 50, 'B02', 'A4', 'Untuk coretan', '2016-02-15'),
+(2, 'Dimpos', 'Mahasiswa', 5, 'A01', 'Spidol Merah', 'Keperluan matkul', '2016-02-15'),
+(4, 'Vincent', 'Mahasiswa', 5, 'A01', 'Spidol Merah', 'a', '2016-02-15'),
+(5, 'Thea', 'Mahasiswa', 10, 'B02', 'A4', 'tes', '2016-02-15');
 
 -- --------------------------------------------------------
 
@@ -76,6 +79,16 @@ CREATE TABLE `t_user` (
   `nama_user` varchar(50) NOT NULL,
   `kategori_user` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_user`
+--
+
+INSERT INTO `t_user` (`id_user`, `nama_user`, `kategori_user`) VALUES
+(1, 'Calvin', 'Mahasiswa'),
+(2, 'Dimpos', 'Mahasiswa'),
+(4, 'Vincent', 'Mahasiswa'),
+(5, 'Thea', 'Mahasiswa');
 
 --
 -- Indexes for dumped tables
@@ -91,7 +104,7 @@ ALTER TABLE `t_atk`
 -- Indexes for table `t_pemakaian`
 --
 ALTER TABLE `t_pemakaian`
-  ADD PRIMARY KEY (`id_pemakaian`);
+  ADD PRIMARY KEY (`id_user`,`kode_barang`,`tanggal`);
 
 --
 -- Indexes for table `t_user`
@@ -104,15 +117,10 @@ ALTER TABLE `t_user`
 --
 
 --
--- AUTO_INCREMENT for table `t_pemakaian`
---
-ALTER TABLE `t_pemakaian`
-  MODIFY `id_pemakaian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
 -- AUTO_INCREMENT for table `t_user`
 --
 ALTER TABLE `t_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
